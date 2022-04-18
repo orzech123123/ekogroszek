1d2980ef22aa088031712b151d22a0cb2ba793fc
Michal Orzechowski <Michal.Orzechowski@BritishCouncil.Org>
2022-04-19 01:19:04 +0200

    Sending sms via smsapi

--- App/Program.cs
+++ App/Program.cs
@@ -1,7 +1,29 @@
-﻿var counter = 0;
+﻿using RestSharp;
+
+
+var smsResponse = await SendSmsAsync("48509980548", "test wiadomosc");
+Console.WriteLine(smsResponse.Content);
+
+var counter = 0;
 var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
 while (max == -1 || counter < max)
 {
     Console.WriteLine($"Counter: {++counter}");
     await Task.Delay(TimeSpan.FromMilliseconds(1_000));
+}
+
+async Task<RestResponse> SendSmsAsync(string to, string message)
+{
+    var client = new RestClient("https://api.smsapi.pl/sms.do");
+
+    var request = new RestRequest();
+    request.AddHeader("Authorization", "Bearer zRi17DqtZUOx3xVdG9ehdeD0bPkon8ze7lCwxcTe");
+    request.AddQueryParameter("to", to);
+    request.AddQueryParameter("message", message);
+    request.AddQueryParameter("test", "1");
+    request.AddQueryParameter("format", "json");
+
+    var response = await client.GetAsync(request);
+
+    return response;
 }
